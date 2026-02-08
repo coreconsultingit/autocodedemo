ead000914e9a6b8f3071121ad96d45fd23a98b76
coreconsultingit <116389752+coreconsultingit@users.noreply.github.com>
2026-02-08 12:50:22 +0000

    [AUT-2] Implement: end to end test

--- tests/e2e/core_functionality.spec.js
+++ tests/e2e/core_functionality.spec.js
@@ -1,13 +1,4 @@
-describe('Core Application Functionality', () => {
-  it('should load the homepage', () => {
-    // Add tests to validate key user flows
-  });
-
-  it('should allow user registration', () => {
-    // Add tests to validate registration process
-  });
-
-  it('should allow user login', () => {
-    // Add tests to validate login process
-  });
-});
+describe('Core Functionality', () => {
+  it('should do X', () => {...})
+  it('should do Y', () => {...})
+})
