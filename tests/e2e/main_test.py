f04db6d502e6fe144f9aa84df3fb661d28bd6e48
coreconsultingit <116389752+coreconsultingit@users.noreply.github.com>
2026-02-08 12:43:08 +0000

    [AUT-2] Implement: end to end test

---
+++ tests/e2e/main_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+class EndToEndTests(unittest.TestCase):
+    def test_login(self):
+        # Test login functionality
+        pass
+
+    def test_create_issue(self):
+        # Test creating a new issue
+        pass
+
+    def test_update_issue(self):
+        # Test updating an existing issue
+        pass
