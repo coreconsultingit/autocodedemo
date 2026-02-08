6515dd7627fed490947b288005c3b0a5b3eca735
coreconsultingit <116389752+coreconsultingit@users.noreply.github.com>
2026-02-08 23:15:09 +0000

    [AUT-3] Implement: Display lable autocode on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,1 +1,16 @@
-<div className='bg-[#e9e9e9] px-4 py-2 rounded-md text-sm font-medium text-gray-900 inline-block'>auto-code-deploy</div>
+
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="bg-gray-100 py-16">
+      <div className="container mx-auto">
+        <div className="bg-blue-500 text-white px-4 py-2 rounded-md inline-block">
+          auto-code-deploy
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
