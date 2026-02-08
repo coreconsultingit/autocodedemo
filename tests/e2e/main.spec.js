d53b2879e452747d2321f59e28c293b274c453cb
coreconsultingit <116389752+coreconsultingit@users.noreply.github.com>
2026-02-08 12:43:06 +0000

    [AUT-2] Implement: end to end test

---
+++ tests/e2e/main.spec.js
@@ -0,0 +1,9 @@
+describe('Main user flow', () => {
+  it('should display homepage', () => {
+    // Implement e2e test for homepage
+  });
+
+  it('should complete checkout', () => {
+    // Implement e2e test for checkout process
+  });
+});
