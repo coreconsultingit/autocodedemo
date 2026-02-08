614642f8a2025510e5cb6036a7066cc7c1967455
coreconsultingit <116389752+coreconsultingit@users.noreply.github.com>
2026-02-08 12:43:13 +0000

    [AUT-2] Implement: end to end test

---
+++ tests/end_to_end_tests.py
@@ -0,0 +1,10 @@
+import unittest
+
+class EndToEndTests(unittest.TestCase):
+    def test_login(self):
+        # Add login test case
+        pass
+
+    def test_create_issue(self):
+        # Add create issue test case
+        pass
