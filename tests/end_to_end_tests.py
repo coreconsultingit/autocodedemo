c5c70b1eb88f656531efa87403f6fd10756560bf
coreconsultingit <116389752+coreconsultingit@users.noreply.github.com>
2026-02-08 12:43:27 +0000

    [AUT-2] Implement: end to end test

--- tests/end_to_end_tests.py
+++ tests/end_to_end_tests.py
@@ -1,10 +1,10 @@
 import unittest
 
 class EndToEndTests(unittest.TestCase):
-    def test_login(self):
-        # Add login test case
+    def test_homepage(self):
+        # Add end-to-end test for homepage
         pass
 
-    def test_create_issue(self):
-        # Add create issue test case
+    def test_login(self):
+        # Add end-to-end test for login functionality
         pass
