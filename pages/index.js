e58f36abca78dadfe3447ce5df3625e0c5b28fc9
coreconsultingit <116389752+coreconsultingit@users.noreply.github.com>
2026-02-08 23:46:39 +0000

    [AUT-1] Implement: Test auto code issue fix

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,24 @@
-import { Dialog } from "@radix-ui/react-dialog";
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "@radix-ui/react-dropdown-menu";
+import { Dialog, DialogTrigger, DialogContent, DialogClose } from "@radix-ui/react-dialog";
+
+export default function Home() {
+  return (
+    <div className="p-4">
+      <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem>Option 1</DropdownMenuItem>
+          <DropdownMenuItem>Option 2</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <Dialog>
+        <DialogTrigger>Open Dialog</DialogTrigger>
+        <DialogContent>
+          <h2 className="text-lg font-medium">Dialog Title</h2>
+          <DialogClose className="absolute top-4 right-4 rounded-full focus:outline-none focus-visible:ring focus-visible:ring-purple-500">Close</DialogClose>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
