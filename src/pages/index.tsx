5ded014b6e1247af4e0a9523da28caa39f25113a
coreconsultingit <116389752+coreconsultingit@users.noreply.github.com>
2026-02-08 23:09:29 +0000

    [AUT-4] Implement: Display an image on the homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
-
 import Image from 'next/image';
-import { Box } from '@radix-ui/react-primitive';
 
 export default function Home() {
   return (
-    <Box className="flex justify-center items-center h-screen">
-      <Image src="/hero.jpg" alt="Hero Image" width={800} height={600} />
-    </Box>
+    <div className="flex justify-center items-center h-screen">
+      <Image src="/hero-image.jpg" alt="Hero Image" width={800} height={600} />
+    </div>
   );
 }
