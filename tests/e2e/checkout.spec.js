8f24e87271d44728103d5bc2cc25352ed040b3bd
coreconsultingit <116389752+coreconsultingit@users.noreply.github.com>
2026-02-08 12:43:58 +0000

    [AUT-2] Implement: end to end test

--- tests/e2e/checkout.spec.js
+++ tests/e2e/checkout.spec.js
@@ -1,1 +1,7 @@
-describe('Checkout', () => { ... });
+import { addToCart, proceedToCheckout, verifyOrder } from './utils';
+
+test('user can complete checkout', () => {
+  addToCart('Product A');
+  proceedToCheckout();
+  verifyOrder();
+});
