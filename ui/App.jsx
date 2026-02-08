8bb4e7e9ff6c2d749af7e26f8a3805878dafaa23
coreconsultingit <116389752+coreconsultingit@users.noreply.github.com>
2026-02-08 12:32:35 +0000

    [AUT-1] Implement: Test auto code issue

--- ui/App.jsx
+++ ui/App.jsx
@@ -1,11 +1,9 @@
 import { Button } from '@shadcn/ui';
 
-function App() {
+export default function App() {
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="p-4">
+      <Button>Hello, world!</Button>
     </div>
   );
 }
-
-export default App;
