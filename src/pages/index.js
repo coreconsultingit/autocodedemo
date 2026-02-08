b87866322dfeb026a7104190335355dd342b8f00
coreconsultingit <116389752+coreconsultingit@users.noreply.github.com>
2026-02-08 23:31:42 +0000

    [AUT-1] Implement: Test auto code issue fix

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from '@radix-ui/react-button';
+
+export default function Home() {
+  return (
+    <div className="p-4">
+      <Button>Click me</Button>
+    </div>
+  );
+}
