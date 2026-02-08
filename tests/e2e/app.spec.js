6dec15b14f5fa7aac71a65d7f6d8f2fb1c37f2d0
coreconsultingit <116389752+coreconsultingit@users.noreply.github.com>
2026-02-08 12:43:55 +0000

    [AUT-2] Implement: end to end test

---
+++ tests/e2e/app.spec.js
@@ -0,0 +1,7 @@
+describe('Application End-to-End Tests', () => {
+  it('should navigate to homepage', () => {...});
+  it('should login successfully', () => {...});
+  it('should create a new item', () => {...});
+  it('should edit an existing item', () => {...});
+  it('should delete an item', () => {...});
+});
