0a0260c847b424f955052096e3655b3c23f84aac
coreconsultingit <116389752+coreconsultingit@users.noreply.github.com>
2026-02-08 23:38:36 +0000

    [AUT-1] Implement: Test auto code issue fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,1 @@
-import { Dialog, DropdownMenu } from '@radix-ui/react-components';
-import { useState } from 'react';
-
-export default function Navbar() {
-  const [open, setOpen] = useState(false);
-
-  return (
-    <header className="bg-white shadow">
-      <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
-        {/* Navbar content */}
-      </nav>
-    </header>
-  );
-}
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
