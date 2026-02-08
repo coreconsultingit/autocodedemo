bafe639c6a845de0a63f89a823a8cecd2d28e69a
coreconsultingit <116389752+coreconsultingit@users.noreply.github.com>
2026-02-08 12:50:33 +0000

    [AUT-2] Implement: end to end test

--- tests/e2e/test_suite.py
+++ tests/e2e/test_suite.py
@@ -1,9 +1,4 @@
 import pytest
 
-def test_login():
-    # End-to-end test for login functionality
-    pass
-
-def test_create_issue():
-    # End-to-end test for creating an issue
-    pass
+def test_homepage():
+    ...
