20272ba54a935453ffa970174242fe177aaccad7
coreconsultingit <116389752+coreconsultingit@users.noreply.github.com>
2026-02-08 23:03:22 +0000

    [AUT-4] Implement: Display an image on the homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <Image src="/hero-image.jpg" alt="Hero Image" width={800} height={600} />
+    <div>
+      <Image src="/hero-image.jpg" alt="Hero Image" width={1200} height={600} />
     </div>
   );
 }
