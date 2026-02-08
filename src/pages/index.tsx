ea0907be90e7632d8070ea478b635a64a4dafead
coreconsultingit <116389752+coreconsultingit@users.noreply.github.com>
2026-02-08 23:03:52 +0000

    [AUT-3] Implement: Display lable autocode on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,1 +1,13 @@
-<div className="bg-blue-500 px-2 py-1 text-white rounded-md">auto-code-deploy</div>
+
+import { Button } from 'tailwindcss-react-ui'
+
+export default function Home() {
+  return (
+    <div>
+      <div className="bg-blue-500 text-white px-4 py-2 rounded-md">
+        auto-code-deploy
+      </div>
+      <Button>Click me</Button>
+    </div>
+  )
+}
