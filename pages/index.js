3a8c436939c030a4d4519d8d97f35771a158fc2d
coreconsultingit <116389752+coreconsultingit@users.noreply.github.com>
2026-02-09 00:10:22 +0000

    [AUT-1] Implement: Test auto code issue fix

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,19 @@
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu';
-import { Dialog, DialogContent, DialogTrigger } from '@radix-ui/react-dialog';
+import Dialog from '@radix-ui/react-dialog';
 
 export default function Home() {
   return (
-    <div className="p-4">
-      <DropdownMenu>
-        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuItem>Option 1</DropdownMenuItem>
-          <DropdownMenuItem>Option 2</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-      <Dialog>
-        <DialogTrigger>Open Dialog</DialogTrigger>
-        <DialogContent>This is a dialog.</DialogContent>
-      </Dialog>
+    <div className="flex justify-center items-center h-screen">
+      <Dialog.Root>
+        <Dialog.Trigger>Open Dialog</Dialog.Trigger>
+        <Dialog.Portal>
+          <Dialog.Overlay className="bg-black/50 fixed inset-0" />
+          <Dialog.Content className="bg-white p-6 rounded-lg shadow-lg fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <Dialog.Title className="text-lg font-bold">Dialog Title</Dialog.Title>
+            <Dialog.Description className="text-gray-500">Dialog content goes here.</Dialog.Description>
+            <Dialog.Close className="mt-4 px-4 py-2 bg-gray-200 rounded hover:bg-gray-300">Close</Dialog.Close>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
     </div>
-  );
+  )
 }
